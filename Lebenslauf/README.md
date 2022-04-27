dfea5327a0d2b329292cf33107b7f805b2acfdc9
Le0X8 <84378319+Le0X8@users.noreply.github.com>
2022-04-27 11:49:52 +0200

    Fehler (!!)
    Angular hat aufgegeben.

--- Lebenslauf/README.md
+++ Lebenslauf/README.md
@@ -1,27 +1,7 @@
-# Lebenslauf
+# Projekt "Lebenslauf"
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.3.
+Dieses Projekt ist im Rahmen eines Praktikums von [Le0_X8](https://github.com/Le0X8) bei [IT-Keller](https://github.com/LarsITK) entstanden.
 
-## Development server
+## Code
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+Der TypeScript-Code ist unter `/src/app/dashboard/dashboard.component.ts` zu finden.
--- Lebenslauf/src/app/dashboard/dashboard.component.ts
+++ Lebenslauf/src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,9 @@
 import { PropertyWrite } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
-var bildungswegfunktion = function (input: string[][]) {
-  var bildungswegtabelle = "<table>"
-  for (var i = 0; i < input.length; i++) {
-    bildungswegtabelle += "<tr><td>" + input[i][0] + "</td><td>" + input[i][1] + "</td></tr>"
-  }
-  bildungswegtabelle += "</table>"
-  return bildungswegtabelle
+function datumHeute() {
+  var heute = new Date()
+  return heute.toLocaleDateString('de-DE', {"day": "2-digit", "month": "short", "year": "numeric"})
 }
 
 @Component({
@@ -15,7 +11,6 @@ var bildungswegfunktion = function (input: string[][]) {
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-
 
 export class DashboardComponent implements OnInit {
 
@@ -40,11 +35,36 @@ export class DashboardComponent implements OnInit {
     ["01/2010-01/2020", "Mustergymnasium Musterstadt"],
     ["01/2005-01/2010", "Grundschule Musterschule in Musterstadt"]
   ]
-
+  erfahrung = []
+  sprachkenntnisse = [
+    ["Deutsch", 0],
+    ["Englisch", 2],
+    ["Französisch", 3]
+  ]
+  computerkenntnisse = [
+    ["Microsoft Word", 2],
+    ["Microsoft PowerPoint", 2],
+    ["Microsoft Excel", 3],    
+    ["GIMP", 2],
+    ["HTML", 2],
+    ["JavaScript", 2],
+    ["CSS", 2],
+    ["Python", 2]
+  ]
+  sonstiges = [
+    "DELF A1-Diplom (Französisch)",
+    "DELF A2-Diplom (Französisch)"
+  ]
+  hobbys = [
+    "Schwimmen",
+    "Klettern",
+    "Programmieren",
+    "Bildbearbeitung"
+  ]
+  datum = datumHeute()
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
-
